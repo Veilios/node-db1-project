0b461500ee66af137940ea6fbbdbf8ef06bd5739
Alan Mir <alanpaulphotos@gmail.com>
2021-03-24 23:20:42 -0400

    Finished checkAccountPayload middleware

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -2,6 +2,19 @@ const User = require('../accounts/accounts-model');
 
 exports.checkAccountPayload = (req, res, next) => {
   // DO YOUR MAGIC
+  try {
+    if(req.body && Object.keys(req.body).length>0) {
+      if(req.body.name && req.body.budget) {
+        next();
+      } else {  
+        res.status(400).json({ message: "Missing required name and/or budget field" });
+      };
+    } else {
+      res.status(400).json({ message: "Missing account data" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Error processing request", error: error });
+  }
 }
 
 exports.checkAccountNameUnique = async (req, res, next) => {
