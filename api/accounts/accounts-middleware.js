23088e87d57747ea9893e9017b5a2e4c5add185e
Alan Mir <alanpaulphotos@gmail.com>
2021-03-24 22:11:22 -0400

    Finished checkAccountId middleware

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,3 +1,5 @@
+const User = require('../accounts/accounts-model');
+
 exports.checkAccountPayload = (req, res, next) => {
   // DO YOUR MAGIC
 }
@@ -8,4 +10,17 @@ exports.checkAccountNameUnique = async (req, res, next) => {
 
 exports.checkAccountId = async (req, res, next) => {
   // DO YOUR MAGIC
-}
+  const {id} = req.params.id;
+  const user = await User.getById(id);
+
+  try {
+    if(user) {
+      req.user = user;
+      next();
+    } else {
+      res.status(404).json({ message: "User not found" });
+    };
+  } catch (err) {
+    res.status(500).json({ message: "Error processing request", error: err});    
+  };
+};
